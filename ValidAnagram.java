df92d73219a4e2b6602be27291cfbe59acea85b4
Shubham1411 <shubhamdutta1411@gmail.com>
2021-07-08 23:34:41 -0700

    String (Valid Anagram)

---
+++ ValidAnagram.java
@@ -0,0 +1,29 @@
+public class ValidAnagram {
+    public static boolean isAnagram(String s, String t) {
+
+        int count [] = new int[26];
+
+        if(s.length() != t.length())
+            return false;
+
+        for(int i=0; i<s.length(); i++){
+            count[s.charAt(i) - 97]++;
+            count[t.charAt(i) - 97]--;
+        }
+
+
+        for(int i=0; i<26; i++)
+            if(count[i] != 0)
+                return false;
+
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        String s = "anagram", t = "nlgaram";
+
+        System.out.println(isAnagram(s,t));
+
+    }
+}
