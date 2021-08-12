00651ad2e1c411246e0959406b5fd43fcc5e1927
sdutta1411 <shubhamdutta1411@gmail.com>
2021-08-12 00:47:32 -0700

    Arrays (Included HashSet Approach)

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -4,9 +4,13 @@ element is distinct.
  */
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContainsDuplicate {
-    public static boolean containsDuplicate(int[] nums) {
+    //Approach 1: Sort the array and compare adjacent elements
+    //Time: 99 percentile, Space: O(n)
+    public static boolean containsDuplicate1(int[] nums) {
 
         if (nums.length == 1)
             return false;
@@ -23,10 +27,25 @@ public class ContainsDuplicate {
         return false;
     }
 
+    //Approach 2: Using HashSet to check the duplicate elements
+    //Time: 55 percentile, Space: O(n)
+    public static boolean containsDuplicate2(int[] nums) {
+
+        Set<Integer> hashset = new HashSet<>();
+
+        for (int num : nums) {
+            if (hashset.contains(num)) return true;
+
+            hashset.add(num);
+        }
+
+        return false;
+    }
+
     public static void main(String[] args) {
 
-        int nums[] = {1, 2, 3, 1};
+        int nums[] = {1, 2, 3, 4};
 
-        System.out.println("Array Contains Duplicate Elements : " + containsDuplicate(nums));
+        System.out.println("Array Contains Duplicate Elements : " + containsDuplicate2(nums));
     }
 }
