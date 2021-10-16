17c05d1f174d4e91148e6697b4934626dcf99a20
sdutta1411 <shubhamdutta1411@gmail.com>
2021-10-15 21:27:39 -0700

    Recursion (Solved Fibonacci using Recursion)

---
+++ FibonacciNumber.java
@@ -0,0 +1,49 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is
+the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+
+Given n, calculate F(n).
+ */
+public class FibonacciNumber {
+
+    //Approach 1: Simple Recursion
+    //Time: 8ms
+    public static int fib1(int n) {
+
+        if (n < 2)
+            return n;
+
+        return fib1(n - 1) + fib1(n - 2);
+
+    }
+
+    //Approach 2: Recursion + memoization
+    //Time: 0ms
+    static Map<Integer, Integer> memo = new HashMap<>();
+
+    public static int fib2(int n) {
+
+        int val = 0;
+        if (memo.containsKey(n))
+            return memo.get(n);
+
+        if (n < 2)
+            return n;
+
+        val = fib2(n - 1) + fib2(n - 2);
+        memo.put(n, val);
+        return memo.get(n);
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(fib2(4));
+    }
+}
