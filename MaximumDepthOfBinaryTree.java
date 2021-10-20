286da64e3a67a1a7fd6ca486c8a385c0f9e6416a
sdutta1411 <shubhamdutta1411@gmail.com>
2021-10-19 20:46:05 -0700

    Added Iterative solution

--- MaximumDepthOfBinaryTree.java
+++ MaximumDepthOfBinaryTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 /*
 Given the root of a binary tree, return its maximum depth.
 A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest
@@ -30,8 +33,9 @@ public class MaximumDepthOfBinaryTree {
     }
 
     //Approach 1: Bottom up approach to recursive call the function the child nodes and calculate the depth of the root
-    //node with the answer returned from the child nodes
-    public int maxDepth(TreeNode root) {
+    //node with the answer returned from the child nodes (DFS)
+    //Time: O(n) 0ms, Space: O(n)
+    public int maxDepth1(TreeNode root) {
 
         //Return 0 if the root node is null
         if (root == null)
@@ -42,12 +46,42 @@ public class MaximumDepthOfBinaryTree {
             return 1;
 
         //Calculate the depth of left child
-        int left_depth = maxDepth(root.left);
+        int left_depth = maxDepth1(root.left);
         //Calculate the depth of right child
-        int right_depth = maxDepth(root.right);
+        int right_depth = maxDepth1(root.right);
 
         //Return max of left depth and right depth + 1
         return Math.max(left_depth, right_depth) + 1;
+    }
+
+    //Approach 2: Solving it iteratively using queue.Not an optimized approach.
+    public int maxDepth2(TreeNode root) {
+
+        if (root == null) return 0;
+
+        Queue<TreeNode> nodes = new LinkedList<>();
+        Queue<Integer> depths = new LinkedList<>();
+
+        nodes.offer(root);
+        depths.offer(1);
+
+        int currentDepth = 0, maxDepth = 0;
+
+        while (!nodes.isEmpty()) {
+
+            TreeNode node = nodes.poll();
+            currentDepth = depths.poll();
+
+            if (node != null) {
+                maxDepth = Math.max(currentDepth, maxDepth);
+                nodes.offer(node.left);
+                nodes.offer(node.right);
+                depths.offer(currentDepth + 1);
+                depths.offer(currentDepth + 1);
+            }
+        }
+
+        return maxDepth;
     }
 
     public static void main(String[] args) {
